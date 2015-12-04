6a549a6560264f10e02979749f5be275ab5fd2cd
iMarv23 <marv.internetz@gmail.com>
2015-12-04 21:13:49 +0100

    Initial commit

---
+++ Scripts/codeParser.js
@@ -0,0 +1,33 @@
+﻿$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+    $('#buttonGenerate').on('click', function () {
+        var bbFormat = $('input[name=bbCodeFormat]:checked').val();
+        var titleTemplate;
+        var regexTab;
+        var regexSubTab;
+        switch (bbFormat) {
+            case '1':
+                regexTab = /\[tab=((\w| )*)\]/g;
+                regexSubTab = /\[subtab=((\w| )*)\]((.|\s)+?((?=\[subtab)|(?=\[\/tabmenu)|(?=\[tab)))/g;
+                titleTemplate = '###';
+                break;
+            case '2':
+                regexTab = /\[tab="((.| )*)"\]/g;
+                regexSubTab = /\[subtab="((.| )*)"\]((.|\s)+?((?=\[subtab)|(?=\[\/tabmenu)|(?=\[tab)))/g;
+                titleTemplate = '"###"';
+                break;
+            case '3':
+                regexTab = /\[tab='((.| )*)'\]/g;
+                regexSubTab = /\[subtab='((.| )*)'\]((.|\s)+?((?=\[subtab)|(?=\[\/tabmenu)|(?=\[tab)))/g;
+                titleTemplate = "'###'";
+                break;
+        }
+        
+        var input = $('#inputArea').val();
+
+        input = input.replace(regexSubTab, '[spoiler=' + titleTemplate.replace('###', '$1') + ']$3[/spoiler]');
+        input = input.replace(regexTab, '[h]$1[/h]');
+        input = input.replace('[tabmenu]', '').replace('[/tabmenu]', '');
+        $('#outputArea').val(input.trim());
+    });
+});
