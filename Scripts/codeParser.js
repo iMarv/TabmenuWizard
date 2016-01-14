3015b100569cee09c11045bb43eb9c6594fa0af2
iMarv23 <marv.internetz@gmail.com>
2016-01-14 13:58:48 +0100

    Minor code improvements

--- Scripts/codeParser.js
+++ Scripts/codeParser.js
@@ -1,22 +1,27 @@
 ﻿$(function () {
-    $('[data-toggle="tooltip"]').tooltip();
+    var BB_FORMAT_TYPES = {
+        NoQuotes: 1,
+        DoubleQuotes: 2,
+        SingleQuotes: 3
+    };
+
     $('#buttonGenerate').on('click', function () {
         var bbFormat = $('input[name=bbCodeFormat]:checked').val();
         var titleTemplate;
         var regexTab;
         var regexSubTab;
         switch (bbFormat) {
-            case '1':
+            case BB_FORMAT_TYPES.NoQuotes:
                 regexTab = /\[tab=((\w|[ äöüÄÖÜ,.\-;:_#'+*!"§$%&/()=?ß^°])*)\]/g;
                 regexSubTab = /\[subtab=((\w|[ äöüÄÖÜ,.\-;:_#'+*!"§$%&/()=?ß^°])*)\]((.|\s)+?((?=\[subtab)|(?=\[\/tabmenu)|(?=\[tab)))/g;
                 titleTemplate = '###';
                 break;
-            case '2':
+            case BB_FORMAT_TYPES.DoubleQuotes:
                 regexTab = /\[tab="((.| )*)"\]/g;
                 regexSubTab = /\[subtab="((.| )*)"\]((.|\s)+?((?=\[subtab)|(?=\[\/tabmenu)|(?=\[tab)))/g;
                 titleTemplate = '"###"';
                 break;
-            case '3':
+            case BB_FORMAT_TYPES.SingleQuotes:
                 regexTab = /\[tab='((.| )*)'\]/g;
                 regexSubTab = /\[subtab='((.| )*)'\]((.|\s)+?((?=\[subtab)|(?=\[\/tabmenu)|(?=\[tab)))/g;
                 titleTemplate = "'###'";
@@ -25,9 +30,11 @@
         
         var input = $('#inputArea').val();
 
-        input = input.replace(regexSubTab, '[spoiler=' + titleTemplate.replace('###', '$1') + ']$3[/spoiler]');
-        input = input.replace(regexTab, '[h]$1[/h]');
-        input = input.replace('[tabmenu]', '').replace('[/tabmenu]', '');
+        input = input.replace(regexSubTab, '[spoiler=' + titleTemplate.replace('###', '$1') + ']$3[/spoiler]')
+                     .replace(regexTab, '[h]$1[/h]')
+                     .replace('[tabmenu]', '')
+                     .replace('[/tabmenu]', '');
+
         $('#outputArea').val(input.trim());
     });
 });
